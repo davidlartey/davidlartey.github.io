73de5a404ef23f9e0c88d545228467e7ba838b9f
David Lartey <dbilovd@gmail.com>
2023-01-02 19:39:02 +0000

    Fixed warnings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -146,7 +146,7 @@ function MobileNavigation(props) {
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                 {
                   menuItems.map((item) => (
-                    <MobileNavItem href={item.href}>{item.title}</MobileNavItem>
+                    <MobileNavItem key={item.title} href={item.href}>{item.title}</MobileNavItem>
                   ))
                 }
               </ul>
@@ -187,7 +187,7 @@ function DesktopNavigation(props) {
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         {
           menuItems.map((item) => (
-            <NavItem href={item.href}>{item.title}</NavItem>
+            <NavItem key={item.title} href={item.href}>{item.title}</NavItem>
           ))
         }
       </ul>
